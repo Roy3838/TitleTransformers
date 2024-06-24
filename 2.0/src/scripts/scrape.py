fab5e6d61ed20e9509cb19559dca4c9cb548409f
Roy3838 <a01283783@itesm.mx>
2024-06-24 17:29:18 -0600

    Reorganize into scripts

--- 2.0/src/scripts/scrape.py
+++ 2.0/src/scripts/scrape.py
@@ -1,6 +1,7 @@
 from googleapiclient.discovery import build
 import json
 import key
+import datetime
 
 # Your API Key
 api_key = key.get_api_key()
@@ -26,6 +27,9 @@ def get_most_viewed_videos(region_code='US', max_results=50):
 
     return videos
 
+# Define the timestamp
+timestamp = datetime.datetime.now().strftime("%Y,%m,%d,%H:%M:%S")
+
 # Get the most viewed videos
 most_viewed_videos = get_most_viewed_videos()
 
@@ -33,8 +37,9 @@ most_viewed_videos = get_most_viewed_videos()
 for video in most_viewed_videos:
     print(f"Title: {video['title']}, Views: {video['views']}")
 
-# Optionally, save the data to a JSON file
-with open('most_viewed_videos.json', 'w') as f:
+# Optionally, save the data to a JSON file with timestamp
+json_name = '2.0/data/' + timestamp + 'most_viewed_videos.json'
+with open(json_name, 'w') as f:
     json.dump(most_viewed_videos, f, indent=4)
 
-print("Data has been saved to 'most_viewed_videos.json'")
+print("Data has been saved to " + json_name)
